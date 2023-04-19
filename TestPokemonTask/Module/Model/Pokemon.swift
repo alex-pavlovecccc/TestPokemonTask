57e90cd13ead5f23b44ad27679adeae9951a8340
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-19 20:15:37 +0300

    create Pokemon ModelData

---
+++ TestPokemonTask/Module/Model/Pokemon.swift
@@ -0,0 +1,49 @@
+//
+//  Pokemon.swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 18.04.2023.
+//
+
+import Foundation
+
+struct PokemonResult: Codable{
+    var results: [PokemonEntity]
+}
+
+struct PokemonEntity: Codable {
+    var url: String
+}
+
+
+struct PokemonDitails: Codable {
+    let height: Int
+    let name: String
+    let sprites: Sprites
+    let weight: Int
+    let species: PokemonSpecies
+    let types: [PokemonType]
+}
+
+struct Sprites: Codable {
+    let backDefault: String
+    let backShiny: String
+    let frontDefault: String
+    let frontShiny: String
+    
+    enum CodingKeys: String, CodingKey {
+        case backDefault = "back_default"
+        case backShiny = "back_shiny"
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+    }
+}
+
+struct PokemonSpecies: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonType: Codable {
+    let type: PokemonSpecies
+}
