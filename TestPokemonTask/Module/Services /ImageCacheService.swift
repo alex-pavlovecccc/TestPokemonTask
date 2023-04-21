8191c0c8435c7465f99e84c82e082541c13dfcfd
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-21 22:47:59 +0300

    add methods to ImageCacheService

--- TestPokemonTask/Module/Services /ImageCacheService.swift
+++ TestPokemonTask/Module/Services /ImageCacheService.swift
@@ -8,5 +8,29 @@
 import UIKit
 
 final class ImageCacheService {
+    let shared = ImageCacheService()
     
+    private let cache: NSCache<NSString, UIImage> = {
+        let cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 50
+        cache.totalCostLimit = 200 * 1024 * 1024
+        return cache
+    }()
+    
+    func save(url: String?, image: UIImage?) {
+        guard let url = url else { return }
+        guard let image = image else { return }
+        
+        self.cache.setObject(image, forKey: NSString(string: url))
+        
+    }
+    
+    func read(url: String?) -> UIImage? {
+        if let url = url {
+            let image = self.cache.object(forKey: NSString(string: url))
+            return image
+        } else {
+            return nil
+        }
+    }
 }
