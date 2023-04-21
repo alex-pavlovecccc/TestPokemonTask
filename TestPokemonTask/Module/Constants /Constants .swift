e6a97c371074f45efc87141d5a45e97ef3900a64
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-21 12:13:22 +0300

    create constant file

---
+++ TestPokemonTask/Module/Constants /Constants .swift
@@ -0,0 +1,16 @@
+//
+//  Constants .swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 21.04.2023.
+//
+
+import Foundation
+
+final class Constants {
+    static private(set) var heightForImage: CGFloat = 120
+    static private(set) var offset: CGFloat = 20
+    static private(set) var offsetForTop: CGFloat = 60
+    static private(set) var height: CGFloat = 120
+    static private(set) var width: CGFloat = 120 
+}
