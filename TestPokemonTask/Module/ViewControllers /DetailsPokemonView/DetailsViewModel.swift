2846a5e75acf7ac5ae9c7b5ad5b6b5603b67b694
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-21 22:25:47 +0300

    implementation DetailsViewModel Protocol in DetailsViewModel class

--- TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewModel.swift
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewModel.swift
@@ -5,4 +5,46 @@
 //  Created by Alexander Pavlovets on 21.04.2023.
 //
 
-import Foundation
+import UIKit
+
+final class DetailsViewModel: NSObject, DetailsViewModelProtocol {
+    
+    //MARK: - property
+    var delegate: DetailsViewModelDelegate?
+    var imageDownLoadService = ImageDownloadService()
+    
+    private var pokemontEntity: PokemonDetails? {
+        didSet {
+            guard let entity = pokemontEntity else { return }
+            delegate?.getPokemonEntity(entity: entity)
+            self.getImage()
+        }
+    }
+    private var networkingService: NetworkingService = NetworkingService()
+
+    //MARK: - computed properties
+    var getPokemonName: String? {
+        guard let name = pokemontEntity?.name else { return ""}
+        return name
+    }
+
+    //MARK: - methods
+    func getPokemonDetailsWith(url: String) {
+        networkingService.getDetailDataWithUrl(url: url, complition: { result  in
+            switch result {
+            case .failure(let error):
+                print("\(error.localizedDescription)")
+            case .success(let entity):
+                self.pokemontEntity = entity
+            }
+        })
+    }
+    
+    func getImage() {
+        guard let strUrl = pokemontEntity?.sprites.frontDefault else { return }
+        let url = URL(string: strUrl)
+        imageDownLoadService.load(url) { image in
+            self.delegate?.setImage(image: image)
+        }
+    }
+}
