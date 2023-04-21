4adcb2f7a8d05734795e768fc7343dec84c4b6af
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-21 22:20:45 +0300

    add methods for DetailsViewModelProtocol

--- TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewModelProtocol.swift
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewModelProtocol.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol DetailsViewModelProtocol: NSObject {
+    var delegate: DetailsViewModelDelegate? { get set }
+    var getPokemonName: String? { get }
+    func getPokemonDetailsWith(url: String)
+    func getImage()
+}
