49d4c6ed835034ad30e2b5ff7bb9652f249add07
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-21 12:14:33 +0300

    create details view controller class with base methods

--- TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewController.swift
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonView/DetailsViewController.swift
@@ -5,4 +5,37 @@
 //  Created by Alexander Pavlovets on 21.04.2023.
 //
 
-import Foundation
+import UIKit
+
+final class DetailsViewController: UIViewController {
+    
+    //MARK: - Property
+    
+    //MARK: - init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupGUI()
+        self.setupConstraints()
+  
+    }
+    
+    //MARK: - GUI
+    private lazy var pokemonMainImage: UIImageView = {
+        let image = UIImageView(frame: .zero)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    //MARK: - Method
+    private func setupGUI() {
+        self.view.addSubview(self.pokemonMainImage)
+    }
+
+    //MARK: - Constraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([self.pokemonMainImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                                     self.pokemonMainImage.heightAnchor.constraint(equalToConstant: Constants.height),
+                                     self.pokemonMainImage.widthAnchor.constraint(equalToConstant: Constants.width),
+                                     self.pokemonMainImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: -Constants.offsetForTop)])
+    }
+}
