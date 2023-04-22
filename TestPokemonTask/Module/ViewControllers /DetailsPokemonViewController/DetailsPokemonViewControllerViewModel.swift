aa97e2f457f1f0c5f97bea5b719c9bec162ecb40
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-22 23:05:37 +0300

    add the method and services of the DetailsPokemonViewControllerViewModel

--- TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewControllerViewModel.swift
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewControllerViewModel.swift
@@ -7,6 +7,44 @@
 
 import UIKit
 
-final class DetailsPokemonViewControllerViewModel {
+final class DetailsPokemonViewControllerViewModel: NSObject, DetailsPokemonViewControllerProtocol {
+    //MARK: - property
+    var delegate: DetailsPokemonViewControllerDelegate?
     
+    var imageDownLoadService = ImageDownloadService()
+    
+    private var pokemontEntity: PokemonDetails? {
+        didSet {
+            guard let entity = pokemontEntity else { return }
+            delegate?.getPokemonEntity(entity: entity)
+            self.getImage()
+        }
+    }
+    private var networkingService: NetworkingService = NetworkingService()
+
+    //MARK: - computed properties
+    var getPokemonName: String? {
+        guard let name = pokemontEntity?.name else { return ""}
+        return name
+    }
+
+    //MARK: - methods
+    func getPokemonDetailsWith(url: String) {
+        networkingService.getDetailDataWithUrl(url: url, complition: { result  in
+            switch result {
+            case .failure(let error):
+                print("\(error.localizedDescription)")
+            case .success(let entity):
+                self.pokemontEntity = entity
+            }
+        })
+    }
+    
+    func getImage() {
+        guard let strUrl = pokemontEntity?.sprites.frontDefault else { return }
+        let url = URL(string: strUrl)
+        imageDownLoadService.load(url) { image in
+            self.delegate?.setImage(image: image)
+        }
+    }
 }
