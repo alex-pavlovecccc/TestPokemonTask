75c5348eed2d4da7edaad4ddc57a916c077dbecc
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-22 23:07:13 +0300

    add methods and outlet to the PokemonTableViewController

--- TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewController.swift
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewController.swift
@@ -9,12 +9,46 @@ import UIKit
 
 class DetailsPokemonViewController: UIViewController {
 
+    //MARK: - property
+    var viewModel: DetailsPokemonViewControllerProtocol = DetailsPokemonViewControllerViewModel()
+    
+    private var pokemonEntity: PokemonDetails? {
+        didSet {
+            guard let name = pokemonEntity?.name else { return }
+            self.nameLabel.text = name
+            guard let height = pokemonEntity?.height else { return }
+            self.heightLable.text = "\(height)"
+            guard let weight = pokemonEntity?.weight else { return }
+            self.widthLabel.text = "\(weight)"
+        }
+    }
+    
+    //MARK: - outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var heightLable: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+   
+    //MARK: - init
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.bind()
     }
 
-    func setter(str: String) {
-        print("\(str)")
+    //MARK: - methods
+    func bind() {
+        self.viewModel.delegate = self
+    }
+}
+
+//MARK: - Extension
+extension DetailsPokemonViewController: DetailsPokemonViewControllerDelegate {
+    func setImage(image: UIImage) {
+        self.pokemonImage.image = image
+    }
+    
+    func getPokemonEntity(entity: PokemonDetails) {
+        self.pokemonEntity = entity
     }
 }
