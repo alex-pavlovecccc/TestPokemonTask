db8def706b85ee1cb3036ff53be2d859d2f0c114
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-22 22:41:20 +0300

    create new file DetailsPokemonViewControllerViewModel

---
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewControllerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailsPokemonViewControllerViewModel.swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 22.04.2023.
+//
+
+import UIKit
+
+final class DetailsPokemonViewControllerViewModel {
+    
+}
