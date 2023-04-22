f1af6fc6944165301b246fa7c032ff6d9c7ca54f
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-22 22:41:44 +0300

    create new DetailsPokemonViewControllerProtocol and DetailsPokemonViewControllerDelegate

---
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewControllerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  DetailsPokemonViewControllerDelegate.swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 22.04.2023.
+//
+
+import UIKit
+
+protocol DetailsPokemonViewControllerDelegate {
+    
+}
---
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewControllerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  DetailsPokemonViewControllerProtocol.swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 22.04.2023.
+//
+
+import UIKit
+
+protocol DetailsPokemonViewControllerProtocol: NSObject {
+    
+}
+
+
