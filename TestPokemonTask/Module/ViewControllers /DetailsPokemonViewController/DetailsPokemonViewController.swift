92e36ded64194be162a7f6b00bcc0dc8aefc59fe
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-22 22:40:47 +0300

    create a new DetailPokemonViewController with the xib file

---
+++ TestPokemonTask/Module/ViewControllers /DetailsPokemonViewController/DetailsPokemonViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsPokemonViewController.swift
+//  TestPokemonTask
+//
+//  Created by Alexander Pavlovets on 22.04.2023.
+//
+
+import UIKit
+
+class DetailsPokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func setter(str: String) {
+        print("\(str)")
+    }
+}
