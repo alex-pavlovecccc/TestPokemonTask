060b8100859872ed91fe94dd7cae27c13a4e878d
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-23 21:09:03 +0300

    implementation of ViewModel in the SavedViewController and add GUI

--- TestPokemonTask/Module/ViewControllers /SavedViewController/SavedViewController .swift
+++ TestPokemonTask/Module/ViewControllers /SavedViewController/SavedViewController .swift
@@ -8,8 +8,44 @@
 import UIKit
 
 final class SavedViewController: UIViewController {
-    //MARK: - init 
+    
+    //MARK: - ViewModel
+    var viewModel: SavedViewControllerViewModelProtocol = SavedViewControllerViewModel()
+    
+    //MARK: - init
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.bind()
+    }
+    
+    //MARK: - GUI
+    private lazy var pokemonTableView: UITableView = {
+        let table = UITableView(frame: .zero)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        return table
+    }()
+    
+    //MARK: - Methods
+    private func bind() {
+        self.viewModel.delegate = self
+    }
+}
+
+extension SavedViewController: SavedViewControllerViewModelDelegate {
+    func reloadData() {
+        self.pokemonTableView.reloadData()
+    }
+}
+
+//MARK: - Extension
+extension SavedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.getPokemonsCount ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
