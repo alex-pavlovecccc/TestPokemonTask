0422c35ab72eb5b31a5b4bd5758c62c38373829f
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-23 20:35:29 +0300

    update CoreDataService

--- TestPokemonTask/Module/Services /CoreDataService.swift
+++ TestPokemonTask/Module/Services /CoreDataService.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import CoreData
+
+final class CoreDataService {
+    
+    //MARK: - managedObjectContext 
+    static var managedObjectContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    // MARK: - Core Data stack
+    
+    static var persistentContainer: NSPersistentCloudKitContainer = {
+        
+        let container = NSPersistentCloudKitContainer(name: "PokemonApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    static func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
