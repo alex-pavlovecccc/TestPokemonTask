444be38e458ff682ef5dc1ce51e546795f5b89d1
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-24 14:28:27 +0300

    update generateViewControllers in the  MainTabBarController class

--- TestPokemonTask/Module/ViewControllers /MainTabBarController/MainTabBarController .swift
+++ TestPokemonTask/Module/ViewControllers /MainTabBarController/MainTabBarController .swift
@@ -16,13 +16,14 @@ final class MainTabBarController: UITabBarController {
     }
     
     //MARK: - Methods
-    private func generateViewControllers(viewController: UIViewController, isLargeTitle: Bool) -> UIViewController {
+    private func generateViewControllers(viewController: UIViewController, isLargeTitle: Bool, image: UIImage?, tag: Int, title: String) -> UIViewController {
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.navigationBar.prefersLargeTitles = isLargeTitle
+        navigationController.tabBarItem = UITabBarItem(title: title, image: image, tag: tag)
         return navigationController
     }
     
     private func setViewControllers() {
-        self.viewControllers = [generateViewControllers(viewController: PokemonTableViewController(), isLargeTitle: true), generateViewControllers(viewController: SavedViewController(), isLargeTitle: true)]
+        self.viewControllers = [generateViewControllers(viewController: PokemonTableViewController(), isLargeTitle: true, image: UIImage(systemName: "list.bullet"), tag: 0, title: "list"), generateViewControllers(viewController: SavedViewController(), isLargeTitle: true, image: UIImage(systemName: "suit.heart"), tag: 0, title: "Favorite")]
     }
 }
