ea5f60cc8e6df1abb2b539b240918b92712b21ae
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2023-04-24 14:50:49 +0300

    update SavedViewController and update TableViewDelegate and DataSource

--- TestPokemonTask/Module/ViewControllers /SavedViewController/SavedViewController .swift
+++ TestPokemonTask/Module/ViewControllers /SavedViewController/SavedViewController .swift
@@ -16,7 +16,12 @@ final class SavedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.bind()
+        self.setupGUI()
+        self.setupConstraints()
     }
+    
+    //MARK: - nib file
+    private var xibCell = UINib(nibName:"PokemonTableViewCell", bundle: nil)
     
     //MARK: - GUI
     private lazy var pokemonTableView: UITableView = {
@@ -24,12 +29,24 @@ final class SavedViewController: UIViewController {
         table.translatesAutoresizingMaskIntoConstraints = false
         table.delegate = self
         table.dataSource = self
+        table.register(xibCell, forCellReuseIdentifier: "PokemonTableViewCell")
         return table
     }()
     
     //MARK: - Methods
     private func bind() {
         self.viewModel.delegate = self
+    }
+    
+    private func setupGUI() {
+        self.view.addSubview(self.pokemonTableView)
+        self.navigationItem.title = "Favorite"
+    }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([self.pokemonTableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                                     self.pokemonTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                                     self.pokemonTableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+                                     self.pokemonTableView.rightAnchor.constraint(equalTo: self.view.rightAnchor)])
     }
 }
 
@@ -42,10 +59,20 @@ extension SavedViewController: SavedViewControllerViewModelDelegate {
 //MARK: - Extension
 extension SavedViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.getPokemonsCount ?? 0
+        return viewModel.pokemons?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCells(type: PokemonTableViewCell.self, indexPath: indexPath)
+        cell.viewModel.getPokemons(entity: (self.viewModel.pokemons?[indexPath.row])!)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constants.heightForCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
